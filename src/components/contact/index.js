6497cac8179f8081e596c60dacb14922e36da545
Alexy Kotliar <kotliar.alex@gmail.com>
2021-07-12 01:04:03 -0400

    added a form to the contact

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {Container,Form,Button } from 'react-bootstrap';
 
 function Contact(){
+    function handleChange(e){
+        console.log(e.target.value);
+    }
     return(
-        <div>
-            <h1>this is the Contact page</h1>
-        </div>
+        <Container>
+<Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Your email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" onBlur={handleChange}/>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Message:</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="dark" type="submit">
+    Submit
+  </Button>
+</Form>
+</Container>
+
     )
 }
 
